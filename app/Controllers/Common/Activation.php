7a93339f9395eb73947c46c640cab0283c9871b6
Sadekur <shadekur.rahman60@gmail.com>
2026-07-06 22:57:30 +0600

    refactor: remove default keyword and badge position settings from activation process

--- app/Controllers/Common/Activation.php
+++ app/Controllers/Common/Activation.php
@@ -17,24 +17,6 @@ class Activation {
      * Plugin activation
      */
     public function activate() {
-        // Set default keyword lists if not exists
-        if (!get_option('cma_positive_keywords')) {
-            update_option('cma_positive_keywords', "good, great, excellent, amazing, wonderful, fantastic, love, happy, perfect, best, awesome, brilliant, outstanding, superb, terrific");
-        }
-        
-        if (!get_option('cma_negative_keywords')) {
-            update_option('cma_negative_keywords', "bad, terrible, awful, horrible, poor, worst, hate, disappointing, disappointing, fail, failed, useless, pathetic, disaster, garbage");
-        }
-        
-        if (!get_option('cma_neutral_keywords')) {
-            update_option('cma_neutral_keywords', "okay, ok, average, decent, fair, moderate, acceptable, reasonable, standard, normal");
-        }
-        
-        // Set default badge position
-        if (!get_option('cma_badge_position')) {
-            update_option('cma_badge_position', 'top');
-        }
-        
         flush_rewrite_rules();
     }
 
